4e0ddd5a25c0f408aff23670a70a6a4056bc1226
Axel <axelwise676@gmail.com>
2024-09-24 16:44:04 +0100

    testing with basic lambda

--- src/iac/modules/lambda/main.tf
+++ src/iac/modules/lambda/main.tf
@@ -1,3 +1,9 @@
+data "archive_file" "zip_file" {
+  type = "zip"
+  source_file = var.path_to_source_file
+  output_path = var.path_to_artifact
+}
+
 resource "aws_lambda_function" "stock_reminder_lambda" {
   filename      = var.path_to_artifact
   function_name = var.function_name
