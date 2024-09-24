02e7e01b218cb8535aa85c718eea7a1feaaacb0e
Axel <axelwise676@gmail.com>
2024-09-24 16:31:17 +0100

    changed zip target in second zip command

--- src/build.sh
+++ src/build.sh
@@ -3,4 +3,4 @@ pip install --target ./package -r requirements.txt
 cd package
 zip -r ../lambda_function.zip .
 cd ../python
-zip -g lambda_function.zip lambda_function.py
+zip -g ../lambda_function.zip lambda_function.py
