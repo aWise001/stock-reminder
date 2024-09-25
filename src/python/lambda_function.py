ffd91f8bbe240ddd7c17a049b2c306d5f360eb66
Axel <axelwise676@gmail.com>
2024-09-25 21:12:00 +0100

    updated url

--- src/python/lambda_function.py
+++ src/python/lambda_function.py
@@ -5,7 +5,7 @@ import os
 
 def lambda_handler(event, context):
     headers = {"X-Aws-Parameters-Secrets-Token": os.environ.get('AWS_SESSION_TOKEN')}
-    secrets_url = "http:localhost:2773/secretsmanager/get?secretId=test-secret"
+    secrets_url = "http://localhost:2773/secretsmanager/get?secretId=test-secret"
     r = requests.get(secrets_url, headers=headers)
     secret = json.loads(r.text)["SecretString"]
     # df = main.main()
